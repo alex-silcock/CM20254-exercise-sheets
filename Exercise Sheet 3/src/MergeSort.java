7592e45df23afaa1355aba9116fbf51ce8c16c36
alex-silcock <alexsilcock13@gmail.com>
2022-11-06 13:58:32 +0000

    MergeSort algorithm

--- Exercise Sheet 3/src/MergeSort.java
+++ Exercise Sheet 3/src/MergeSort.java
@@ -1,17 +1,39 @@
 public class MergeSort {
 
-    void sort(int[] arr){
-        for (int i = 1; i < arr.length; i++){
-            int current = arr[i];
-            int j = i-1;
+    public void mergeSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
 
-            while (j >= 0 && arr[j] > current){
-                arr[j + 1] = arr[j];
-                j--;
+        System.arraycopy(a, 0, l, 0, mid);
+        if (n - mid >= 0) System.arraycopy(a, mid, r, 0, n - mid);
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+        merge(a, l, r, mid, n - mid);
+    }
+
+    public static void merge(int[] n, int[] l, int[] r, int left, int right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                n[k++] = l[i++];
             }
-            arr[j + 1] = current;
+            else {
+                n[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            n[k++] = l[i++];
+        }
+        while (j < right) {
+            n[k++] = r[j++];
         }
     }
+
 
     static void printArray(int[] arr) {
         for (int j : arr) {
@@ -23,7 +45,7 @@ public class MergeSort {
     public static void main(String[] args) {
         int[] a = {5, 9, 3, 1, 7};
         MergeSort ms = new MergeSort();
-        ms.sort(a);
+        ms.mergeSort(a, a.length);
         printArray(a);
     }
 }
